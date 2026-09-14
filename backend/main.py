221c61913d0ddc72dca03c396213d9015e19a1f6
KemonoBAT4 <76810839+KemonoBAT4@users.noreply.github.com>
2026-09-14 18:50:22 +0000

    started to implement the server in FastAPI using SQLAlchemy and SQLModel

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,51 @@
 # NOTE: see this chat for basic infrastructure
-"https://chatgpt.com/uc/6aa7c636-075c-83ea-9c94-3a022ee5634e"
+
+# DEPS:
+# install fastapi
+# install sqlalchemy
+# install sqlmodel
+
+class User(..., table=True):
+    username: str
+    
+    name: str
+    surname: str
+
+    email: str
+    phone: str
+
+    profile_picture_path: str # the path to the cdn for the pfp file
+
+    setting_uname: str = ... # reference to the settings model for app settings (app theme, ecc)
+    setting: Setting = ...
+
+    status: UserStatusEnum = ... # the status enumerative (active, deactivated, banned, ecc...)
+# #endclass User
+
+class Setting(..., table=True):
+    ...
+# #endclass Setting
+
+"""
+# TODO:
+- [ ] implement fastapi
+    - [ ] all the get / post routes
+    - [ ] all the socket connections for the chat
+- [ ] implement docker w/ the db
+    - [ ] use postgreSQL for the db
+
+## MODELS TO CREATE:
+- [ ] User
+    - one user can have more than one permissions & roles
+    - one user can have more than one groups & chats
+- [ ] Permission
+
+- [ ] Roles
+
+- [ ] Groups
+    - [ ] Group members
+
+- [ ] Chat
+
+- [ ] Setting
+"""
